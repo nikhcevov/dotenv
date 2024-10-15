641dbf0583bb53f68764a06bc4e5405866d740f1
Vladimir Ovechkin <vladimir.ovechkin-extern@marketlogicsoftware.com>
2024-10-15 21:51:30 +0300

    Update format action

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,7 +5,7 @@ return {
 	keys = {
 		{
 			-- Customize or remove this keymap to your liking
-			"<leader>ft",
+			"<leader>f",
 			function()
 				require("conform").format({ async = true })
 			end,
